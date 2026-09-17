1bb8911308c49e4a21345966b084ceec39b9b953
Hoàng Bảo Phúc <h.baophuc297@gmail.com>
2026-09-17 20:37:28 +0700

    feat(contracts): add core observation, action and step models

---
+++ src/agentqa/contracts/__init__.py
@@ -0,0 +1,15 @@
+from agentqa.contracts.core import (
+    Action,
+    ActionType,
+    ObservationMode,
+    ObservationSnapshot,
+    StepResult,
+)
+
+__all__ = [
+    "Action",
+    "ActionType",
+    "ObservationMode",
+    "ObservationSnapshot",
+    "StepResult",
+]
